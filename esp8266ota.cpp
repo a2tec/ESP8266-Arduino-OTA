55430ccd9b0495c75790d63f0b5344bb503b41b3
Tianyu Liu <info@jamesvillage.dev>
2020-10-14 22:46:45 +0200

    Add ability to disable timer

--- esp8266ota.cpp
+++ esp8266ota.cpp
@@ -28,6 +28,38 @@ Esp8266OTA::Esp8266OTA(String hostname, String password)
     updater.begin();
 }
 
+Esp8266OTA::Esp8266OTA(String hostname, String password, std::function<void(void)> timerDetatchFunction)
+    : updater()
+    , m_timerDetatch(timerDetatchFunction)
+{
+    // std::bind()
+    m_timerDetatch = timerDetatchFunction;
+    updater.setHostname(hostname.c_str());
+    updater.setPassword(password.c_str());
+
+    updater.onStart([&]() {
+        m_timerDetatch();
+        Serial.println("Start");
+    });
+    updater.onEnd([]() {
+        Serial.println("\nEnd");
+    });
+
+    updater.onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+
+    updater.onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+        else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+        else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+        else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+    updater.begin();
+}
+
 void Esp8266OTA::handle()
 {
     updater.handle();
--- esp8266ota.h
+++ esp8266ota.h
@@ -3,13 +3,17 @@
 #include <ArduinoOTA.h>
 #include <string>
 #include <ESP8266mDNS.h>
+#include <vector>
 
 class Esp8266OTA
 {
 public:
     Esp8266OTA(String hostname, String password);
+    Esp8266OTA(String hostname, String password, std::function<void(void)> timerDetatchFunction);
 
     void handle();
 private:
     ArduinoOTAClass updater;
+    std::function<void(void)> m_timerDetatch;
+    std::function<void(uint8_t)> m_gpioDetatch;
 };
